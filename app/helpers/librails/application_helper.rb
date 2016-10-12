f9f84d4637076ebb22b44febdb0040d216a47236
src <src@srcw.net>
2016-10-12 15:14:58 +0900

    changes

--- app/helpers/librails/application_helper.rb
+++ app/helpers/librails/application_helper.rb
@@ -1,7 +1,32 @@
+# coding: utf-8
 module Librails
   module ApplicationHelper
-    def hello3
-      "Hello3"
+    def bootstrap_class_for(flash_type)
+      { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+    end
+
+    # noinspection RubyUnusedLocalVariable
+    def flash_messages(opts = {})
+      # https://github.com/plataformatec/devise/issues/1777
+      # deviseが:timeoutにtrueをぶちこんでくるのでそれを除外する
+      flash.each do |msg_type, message|
+        next if msg_type == :timedout
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
+                 concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
+                          concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
+                          concat content_tag(:span, 'Close', class: 'sr-only')
+                        end)
+                 concat message
+               end)
+      end
+      nil
+    end
+
+    def ransack_value(params, value_para, mquery_param = 'q')
+      q = params[query_param]
+      return nil unless q
+      return nil unless q.kind_of?(Hash)
+      q[value_param]
     end
   end
 end
--- lib/librails/version.rb
+++ lib/librails/version.rb
@@ -1,3 +1,3 @@
 module Librails
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
