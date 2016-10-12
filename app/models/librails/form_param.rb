6ee150301d36d2f484e553981215a426979fa46f
src <src@srcw.net>
2016-10-13 02:15:04 +0900

    changes

--- app/models/librails/form_param.rb
+++ app/models/librails/form_param.rb
@@ -1,13 +1,15 @@
 # coding: utf-8
-class FormParam
-  def initialize(id, label = nil, value = nil)
-    # 基本はIDとlabel。valueは特別な値を紐付けたい場合にのみ使用する。
-    @id = id
-    @label = label
-    if @label.nil?
-      @label = @id
+module Librails
+  class FormParam
+    def initialize(id, label = nil, value = nil)
+      # 基本はIDとlabel。valueは特別な値を紐付けたい場合にのみ使用する。
+      @id = id
+      @label = label
+      if @label.nil?
+        @label = @id
+      end
+      @value = value
     end
-    @value = value
+    attr_accessor :id, :label, :value
   end
-  attr_accessor :id, :label, :value
 end
