402615dd93213357e10f7bdad11b488902cf127d
src <src@srcw.net>
2023-04-11 14:39:21 +0900

    Bootstrap 5

--- app/helpers/librails/application_helper.rb
+++ app/helpers/librails/application_helper.rb
@@ -5,7 +5,6 @@ module Librails
       { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
     end
 
-    # noinspection RubyUnusedLocalVariable
     def flash_messages(opts = {})
       # https://github.com/plataformatec/devise/issues/1777
       # deviseが:timeoutにtrueをぶちこんでくるのでそれを除外する
@@ -22,6 +21,19 @@ module Librails
       nil
     end
 
+    def bs5_flash_messages(opts = {})
+      # https://github.com/plataformatec/devise/issues/1777
+      # deviseが:timeoutにtrueをぶちこんでくるのでそれを除外する
+      flash.each do |msg_type, message|
+        next if msg_type == :timedout
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
+          concat(content_tag(:button, class: 'btn-close', data: { bs_dismiss: 'alert' }, aria_label: 'Close') do
+          end)
+          concat message
+        end)
+      end
+      nil
+    end
     def ransack_value(params, value_param, query_param = 'q')
       q = params[query_param]
       return nil unless q
