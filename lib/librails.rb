073e0c156ee6a90281e211fe14f11bc9a981764b
src256 <src@srcw.net>
2023-11-07 13:36:04 +0900

    update to Rails 7.0

--- lib/librails.rb
+++ lib/librails.rb
@@ -1,7 +1,8 @@
 require "librails/engine"
-#require "librails/version"
+require "librails/version"
 require "librails/misc_utils"
 require "librails/model_utils"
+require "librails/view_helper"
 
 module Librails
 end
--- lib/librails/version.rb
+++ lib/librails/version.rb
@@ -1,3 +1,3 @@
 module Librails
-  VERSION = "0.3.0"
+  VERSION = "1.0.0"
 end
